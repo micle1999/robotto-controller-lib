f0b05963c19477cdbd65d3a60d800ee21b14c4d7
kwasil <krzysztof@brincker-monitoring.com>
2023-11-27 13:54:48 +0100

    On branch controller-testing  Changes to be committed: 	modified:   src/main/java/robotto/controller/lib/App.java 	modified:   src/main/java/robotto/controller/lib/BlobStorage/Interfaces/BlobStorageService.java

--- src/main/java/robotto/controller/lib/App.java
+++ src/main/java/robotto/controller/lib/App.java
@@ -1,13 +1,30 @@
 package robotto.controller.lib;
 
+import robotto.controller.lib.BlobStorage.Interfaces.BlobStorageService;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
+
+        // Initialize necessary components
+        BlobStorageService blobStorageService = new BlobStorageServiceImpl(); // Initialize BlobStorageService implementation
+        /* 
+        String missionId = "123"; // Example mission ID
+        Resource resource = new Resource(); // Example resource object
+        
+        int resourceId = blobStorageService.uploadResource(missionId, resource);
+        */
+
         System.out.println( "Hello World!" );
+
+        if (resourceId != -1) {
+            System.out.println("Resource uploaded successfully with ID: " + resourceId);
+        } else {
+            System.out.println("Failed to upload the resource.");
+        }
     }
 }
--- src/main/java/robotto/controller/lib/BlobStorage/Interfaces/BlobStorageService.java
+++ src/main/java/robotto/controller/lib/BlobStorage/Interfaces/BlobStorageService.java
@@ -3,7 +3,8 @@ package robotto.controller.lib;
 public interface BlobStorageService {
 
     //mission Id will represent container in blob storage
-    int uploadResource(String missionId, Resource resource); //returns resource id for metadata storage
+    //int uploadResource(String missionId, Resource resource); //returns resource id for metadata storage
 
+    
 
 }
