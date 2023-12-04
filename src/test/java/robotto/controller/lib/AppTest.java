f7134ad7b601ad71ce4d1995e30ca854b0bb40f9
Michal Šalaga <michalsalaga@MichalMacBook.local>
2023-12-04 17:14:39 +0100

    create mission method added to the controller

--- src/test/java/robotto/controller/lib/AppTest.java
+++ src/test/java/robotto/controller/lib/AppTest.java
@@ -3,12 +3,31 @@ package robotto.controller.lib;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import robotto.controller.lib.EventStream.EventPublisher;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest 
 {
+
+    @Autowired
+    private EventPublisher eventPublisher;
+
+    // Context loading test - a simple test that the application context loads successfully
+    @Test
+    public void contextLoads() {
+        assertNotNull(eventPublisher);
+    }
     /**
      * Rigorous Test :-)
      */
